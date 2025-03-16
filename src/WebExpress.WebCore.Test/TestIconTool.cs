9dc464444491f03674a26ced0d062e32c7499a70
Rene Schwarzer <rene_schwarzer@hotmail.de>
2025-03-16 12:19:01 +0100

    feat: improve web icons

--- src/WebExpress.WebCore.Test/TestIconTool.cs
+++ src/WebExpress.WebCore.Test/TestIconTool.cs
@@ -15,8 +15,13 @@ namespace WebExpress.WebCore.Test
         /// </summary>
         /// <param name="renderContext">The context in which the icon is rendered.</param>
         /// <param name="visualTree">The visual tree representing the icon's structure.</param>
+        /// <param name="id">The id attribute of the HTML element.</param>
+        /// <param name="description">The description of the icon.</param>
+        /// <param name="css">The CSS class of the HTML element.</param>
+        /// <param name="style">The inline style of the HTML element.</param>
+        /// <param name="role">The ARIA role of the HTML element.</param>
         /// <returns>An HTML node representing the rendered icon.</returns>
-        public IHtmlNode Render(IRenderContext renderContext, IVisualTree visualTree)
+        public IHtmlNode Render(IRenderContext renderContext, IVisualTree visualTree, string id = null, string description = null, string css = null, string style = null, string role = null)
         {
             return new HtmlElementTextSemanticsSpan(new HtmlText("🛠"));
         }
--- src/WebExpress.WebCore/WebIcon/IIcon.cs
+++ src/WebExpress.WebCore/WebIcon/IIcon.cs
@@ -13,7 +13,12 @@ namespace WebExpress.WebCore.WebIcon
         /// </summary>
         /// <param name="renderContext">The context in which the icon is rendered.</param>
         /// <param name="visualTree">The visual tree representing the icon's structure.</param>
+        /// <param name="id">The id attribute of the HTML element.</param>
+        /// <param name="description">The description of the icon.</param>
+        /// <param name="css">The CSS class of the HTML element.</param>
+        /// <param name="style">The inline style of the HTML element.</param>
+        /// <param name="role">The ARIA role of the HTML element.</param>
         /// <returns>An HTML node representing the rendered icon.</returns>
-        IHtmlNode Render(IRenderContext renderContext, IVisualTree visualTree);
+        IHtmlNode Render(IRenderContext renderContext, IVisualTree visualTree, string id = null, string description = null, string css = null, string style = null, string role = null);
     }
 }
