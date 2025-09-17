20ee76f91ec394f9df17cc31ff44461a57768e26
Rene Schwarzer <rene_schwarzer@hotmail.de>
2025-09-17 22:26:36 +0200

    feat: add fragment page, general improvements and minor bugs

--- src/WebExpress.WebCore/WebAttribute/ScopeAttribute.cs
+++ src/WebExpress.WebCore/WebAttribute/ScopeAttribute.cs
@@ -4,10 +4,16 @@ using WebExpress.WebCore.WebScope;
 namespace WebExpress.WebCore.WebAttribute
 {
     /// <summary>
-    /// The range in which the component is valid.
+    /// Specifies the scope type associated with a class. This attribute can be applied to classes 
+    /// to define their scope, allowing for contextual behavior or configuration based on the 
+    /// specified scope.
     /// </summary>
+    /// <typeparam name="TScope">
+    /// The type of the scope. Must be a class that implements the <see cref="IScope"/> interface.
+    /// </typeparam>
     [AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
-    public class ScopeAttribute<T> : Attribute, IPageAttribute, ISettingPageAttribute where T : class, IScope
+    public class ScopeAttribute<TScope> : Attribute, IPageAttribute, ISettingPageAttribute
+        where TScope : class, IScope
     {
         /// <summary>
         /// Initializes a new instance of the class.
--- src/WebExpress.WebCore/WebAttribute/SectionAttribute.cs
+++ src/WebExpress.WebCore/WebAttribute/SectionAttribute.cs
@@ -4,10 +4,15 @@ using WebExpress.WebCore.WebSection;
 namespace WebExpress.WebCore.WebAttribute
 {
     /// <summary>
-    /// Attribute to identify a section.
+    /// Specifies a section type for a class, allowing the class to be associated with a 
+    /// specific configuration or settings section.
     /// </summary>
+    /// <typeparam name="TSection">
+    /// The type of the section associated with the class. Must be a reference type that 
+    /// implements <see cref="ISection"/>.
+    /// </typeparam>
     [AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
-    public class SectionAttribute<T> : Attribute, IFragmentAttribute, ISettingCategoryAttribute, ISettingGroupAttribute where T : class, ISection
+    public class SectionAttribute<TSection> : Attribute, IFragmentAttribute, ISettingCategoryAttribute, ISettingGroupAttribute where TSection : class, ISection
     {
         /// <summary>
         /// Initializes a new instance of the class.
